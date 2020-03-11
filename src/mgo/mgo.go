7e172f7060064b4d35fd78873ef86de9928597cb
yong.wang <yong.wang@ucarinc.com>
2020-03-11 11:19:04 +0800

    第一章练习题

--- src/mgo/mgo.go
+++ src/mgo/mgo.go
@@ -1,14 +1,123 @@
 package mgo
 
 import (
+	"context"
 	"fmt"
 	"log"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// MongoDB
-func GoMongo() {
+// 定义Student数据结构
+type Student struct {
+	Name string
+	Age  int
+}
 
-	fmt.Println("Go MongoDB ...")
-	log.Printf("%s\n", "Go log ...")
+func GoMongo() {
+	fmt.Println("Hello tgs!")
+	log.Printf("go mongodb ")
+
+	//设置客户短连接配置
+	clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017")
+
+	//连接到MongoDB
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 检查连接
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB!")
+
+	// 指定获取操作的数据集
+	collection := client.Database("qlmi").Collection("student")
+
+	s1 := Student{"小红", 12}
+	s2 := Student{"小兰", 10}
+	s3 := Student{"小黄", 11}
+
+	insertResult, err := collection.InsertOne(context.TODO(), s1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	students := []interface{}{s2, s3}
+	insertManyResult, err := collection.InsertMany(context.TODO(), students)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Inserted multiple documents: ", insertManyResult.InsertedIDs)
+
+	filter := bson.D{{"name", "小兰"}}
+
+	update := bson.D{
+		{"$inc", bson.D{
+			{"age", 1},
+		}},
+	}
+	updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Matched %v documents and updated %v documents.\n", updateResult.MatchedCount, updateResult.ModifiedCount)
+
+	filter = bson.D{{"name", "张三"}}
+	// 创建一个Student变量用来接收查询的结果
+	var result Student
+	err = collection.FindOne(context.TODO(), filter).Decode(&result)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Found a single document: %+v\n", result)
+
+	// 查询多个
+	// 将选项传递给Find()
+	findOptions := options.Find()
+	findOptions.SetLimit(2)
+
+	// 定义一个切片用来存储查询结果
+	var results []*Student
+
+	// 把bson.D{{}}作为一个filter来匹配所有文档
+	cur, err := collection.Find(context.TODO(), bson.D{{}}, findOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 查找多个文档返回一个光标
+	// 遍历游标允许我们一次解码一个文档
+	for cur.Next(context.TODO()) {
+		// 创建一个值，将单个文档解码为该值
+		var elem Student
+		err := cur.Decode(&elem)
+		if err != nil {
+			log.Fatal(err)
+		}
+		results = append(results, &elem)
+	}
+
+	if err := cur.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// 完成后关闭游标
+	cur.Close(context.TODO())
+	fmt.Printf("Found multiple documents (array of pointers): %#v\n", results)
+
+	// 断开连接
+	err = client.Disconnect(context.TODO())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connection to MongoDB closed.")
 
 }
